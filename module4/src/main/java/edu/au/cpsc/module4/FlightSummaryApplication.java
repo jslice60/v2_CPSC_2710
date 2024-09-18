f8d78feb75aeebaac0ca20b39a6f412f6b8a642f
jslice60 <jim.manuel6074@gmail.com>
2024-09-18 02:02:37 -0400

    Bug Toggle Fix

--- module4/src/main/java/edu/au/cpsc/module4/FlightSummaryApplication.java
+++ module4/src/main/java/edu/au/cpsc/module4/FlightSummaryApplication.java
@@ -11,7 +11,7 @@ public class FlightSummaryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FlightSummaryApplication.class.getResource("flight-summary-app.fxml")); //JIMMY fix this!!
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 700);
         stage.setTitle("Flight Schedule App!");
         stage.setScene(scene);
         stage.show();
