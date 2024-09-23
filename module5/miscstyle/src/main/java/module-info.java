b1d5a306de3088b7cba717f3d46a7088e4a44453
jslice60 <jim.manuel6074@gmail.com>
2024-09-22 22:43:55 -0400

    First part of module5 project complete.

---
+++ module5/miscstyle/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module edu.au.cpsc.miscstyle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.au.cpsc.miscstyle to javafx.fxml;
+    exports edu.au.cpsc.miscstyle;
+
+}
